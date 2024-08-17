8cc53baf7a6f20233b599ba2588493906e1d7e79
Zzzz <3407481422@qq.com>
2024-08-17 23:14:02 +0800

    完整的生成

--- Assets/Script/Cup.cs
+++ Assets/Script/Cup.cs
@@ -12,32 +12,48 @@ public class Cup : MonoBehaviour
     /// </summary>
     public GameObject waterPrefab = null;
 
+    /// <summary>
+    /// 这个杯子里有的颜色
+    /// </summary>
+
+    private Color[] hasColor = { };
+
     // Start is called before the first frame update
     void Start()
     {
-        CreatWater();
+        // CreatWater();
     }
 
     /// <summary>
-    /// 生成方块
+    /// 初始生成方块
     /// </summary>
-    private void CreatWater()
+    public void CreatWater(List<Color> colorArray)
     {
         // 生成4个方块
-        for (int i = 0; i < 4; i++)
+        // for (int i = 0; i < 4; i++)
+        // {
+        //     Vector3 newPos = transform.position + new Vector3(0, Global.FIRST_Y + Global.WATER_HEIGHT * i, 0);
+        //     GameObject newWater = Instantiate(waterPrefab, newPos, transform.rotation, transform);
+
+        //     Debug.Log("[zzzz]生成了一个方块，位置是：" + newPos);
+        //     // StartCoroutine("SetColor", newWater);
+        //     int seed = (int)DateTime.Now.Ticks + i;
+        //     Debug.Log("随机种子是:" + seed);
+        //     System.Random random = new System.Random(seed);
+        //     int randomInt = random.Next(0, 4);
+        //     Color newColor = Global.WATER_COLOR[randomInt];
+
+        //     newWater.GetComponent<SpriteRenderer>().color = newColor;
+        // }
+
+        int n = 0;
+        foreach (var color in colorArray)
         {
-            Vector3 newPos = transform.position + new Vector3(0, Global.FIRST_Y + Global.WATER_HEIGHT * i, 0);
+            Vector3 newPos = transform.position + new Vector3(0, Global.FIRST_Y + Global.WATER_HEIGHT * n, 0);
             GameObject newWater = Instantiate(waterPrefab, newPos, transform.rotation, transform);
-
-            Debug.Log("[zzzz]生成了一个方块，位置是：" + newPos);
-            // StartCoroutine("SetColor", newWater);
-            int seed = (int)DateTime.Now.Ticks + i;
-            Debug.Log("随机种子是:" + seed);
-            System.Random random = new System.Random(seed);
-            int randomInt = random.Next(0, 4);
-            Color newColor = Global.WATER_COLOR[randomInt];
-
-            newWater.GetComponent<SpriteRenderer>().color = newColor;
+            int seed = (int)DateTime.Now.Ticks + n;
+            newWater.GetComponent<SpriteRenderer>().color = color;
+            n++;
         }
 
     }
---
+++ Assets/Script/Level.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    /// <summary>
+    /// 放水的杯子数量
+    /// </summary>
+    public int cupCount = 4;
+
+    public GameObject[] cups = { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initCup();
+    }
+
+    /// <summary>
+    /// 初始生成杯子里的水
+    /// </summary>
+    private void initCup()
+    {
+        List<Color> allColors = new List<Color> { };
+
+        // 每个元素复制cupCount次
+        foreach (var item in Global.WATER_COLOR)
+        {
+            for (int i = 0; i < cupCount; i++)
+            {
+                allColors.Add(item);
+                Debug.Log("[zzzz]allColors添加了：" + item);
+            }
+        }
+
+        // 打乱颜色顺序
+        // 使用Fisher-Yates洗牌算法打乱List的顺序
+        System.Random random = new System.Random();
+        int n = allColors.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            Color value = allColors[k];
+            allColors[k] = allColors[n];
+            allColors[n] = value;
+        }
+
+        GameObject cup = GameObject.Find("Cup");
+        for (int i = 0; i < cupCount; i++)
+        {
+            cup.transform.GetChild(i).gameObject.GetComponent<Cup>().CreatWater(allColors.GetRange(i * 4, 4));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
